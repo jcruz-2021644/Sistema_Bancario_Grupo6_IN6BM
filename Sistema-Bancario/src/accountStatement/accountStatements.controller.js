748c966a6747c05fb0a676105dd8e5a7905e8241
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-02-13 20:23:50 -0600

    Fix: Se modifico el metodo listar para la entidad accountStatements

--- Sistema-Bancario/src/accountStatement/accountStatements.controller.js
+++ Sistema-Bancario/src/accountStatement/accountStatements.controller.js
@@ -33,42 +33,49 @@ export const createAccountStatement = async (req, res) => {
         })
     }
 }
-
 export const getAccountStatements = async (req, res) => {
     try {
         const { page = 1, limit = 10, accountId } = req.query;
-        const filter = { accountId };
-        const options = {
-            page: parseInt(page),
-            limit: parseInt(limit),
-            sort: { createdAt: -1 }
+
+        const filter = {};
+
+        if (accountId) {
+            if (!mongoose.Types.ObjectId.isValid(accountId)) {
+                return res.status(400).json({
+                    success: false,
+                    message: "accountId inválido"
+                });
+            }
+
+            filter.accountId = new mongoose.Types.ObjectId(accountId);
         }
 
         const accountStatements = await AccountStatement.find(filter)
-            .limit(limit * 1)
+            .limit(parseInt(limit))
             .skip((page - 1) * limit)
-            .sort(options.sort);
+            .sort({ createdAt: -1 });
+
         const total = await AccountStatement.countDocuments(filter);
 
         res.status(200).json({
             success: true,
             data: accountStatements,
             pagination: {
-                currentPage: page,
+                currentPage: parseInt(page),
                 totalPages: Math.ceil(total / limit),
                 totalRecords: total,
-                limit
+                limit: parseInt(limit)
             }
-        })
+        });
+
     } catch (error) {
         res.status(500).json({
             success: false,
-            message: 'Error al mandar los estados de cuenta',    
+            message: 'Error al mandar los estados de cuenta',
             error: error.message
-        })
+        });
     }
-}
-
+};
 export const updateAccountStatement = async (req, res) => {
     try {
         const { id } = req.params;
