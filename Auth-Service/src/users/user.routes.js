00aef5b0c2dd75aea532b5780ebb2d38c82f3da2
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-02-16 18:24:40 -0600

    Feat: Actualización de controller y rutas

--- Auth-Service/src/users/user.routes.js
+++ Auth-Service/src/users/user.routes.js
@@ -3,7 +3,6 @@ import {
     updateUserRole,
     getUserRoles,
     getUsersByRole,
-    forceUpdateUserRole,
 } from './user.controller.js';
 
 const router = Router();
@@ -17,5 +16,4 @@ router.get('/:userId/roles', ...getUserRoles);
 // GET /api/v1/users/by-role/:roleName
 router.get('/by-role/:roleName', ...getUsersByRole);
 
-router.put('/:userId/force-role', ...forceUpdateUserRole);
 export default router;
