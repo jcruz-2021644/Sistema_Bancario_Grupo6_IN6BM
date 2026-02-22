1df132ab42b0137627fe62035ceb8231cb41e8e4
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-02-22 17:01:30 -0600

    Feat: se crearon funciones para transacciones actualizando saldo y registrando saldo anterior/nuevo

--- Sistema-Bancario/src/transaction/transaction.controller.js
+++ Sistema-Bancario/src/transaction/transaction.controller.js
@@ -1,21 +1,59 @@
 import Transaction from './transaction.model.js';
+import Account from '../accounts/accounts.model.js';
+import {
+    normalizeTransactionData,
+    validateAccountNumberFormat,
+    validateCurrencyForTransaction,
+    applyTransactionBalances
+} from '../../helpers/transaction.helper.js';
 
 //agregar
 export const createTransaction = async (req, res) => {
     try {
 
-        const transactionData = req.body;
+        const transactionData = normalizeTransactionData(req.body);
+        const { sourceAccountNumber, destinationAccountNumber } = transactionData;
 
-        /* if(req.file){
-             const extension = req.file.path.split('.').pop();
-             const filename = req.file.filename;
-             const relativePath = filename.substring(filename.indexOf('fields/'));
-         
-             fieldData.photo = `$(relativePath).$(extension)`;
-         }else{
-             fieldData.photo = 'fields/kinal_sports_nyvxo5';
-         }
- */
+        validateAccountNumberFormat(sourceAccountNumber, 'sourceAccountNumber');
+        validateAccountNumberFormat(destinationAccountNumber, 'destinationAccountNumber');
+
+        if (!transactionData.executedByUserId) {
+            throw new Error('El usuario que ejecuta la transaccion es requerido');
+        }
+
+        if (sourceAccountNumber === destinationAccountNumber && transactionData.transactionType === 'transferencia') {
+            throw new Error('La cuenta origen y destino no pueden ser la misma en una transferencia');
+        }
+
+        const [sourceAccount, destinationAccount] = await Promise.all([
+            Account.findOne({ accountNumber: sourceAccountNumber }),
+            Account.findOne({ accountNumber: destinationAccountNumber })
+        ]);
+
+        if (!sourceAccount || !destinationAccount) {
+            return res.status(404).json({
+                success: false,
+                message: 'Una o ambas cuentas no existen'
+            });
+        }
+
+        await validateCurrencyForTransaction(transactionData.currencyCode, sourceAccount, destinationAccount);
+
+        const { previousBalance, newBalance } = applyTransactionBalances({
+            transactionType: transactionData.transactionType,
+            amount: Number(transactionData.amount),
+            sourceAccount,
+            destinationAccount
+        });
+
+        transactionData.previousBalance = previousBalance;
+        transactionData.newBalance = newBalance;
+
+        await Promise.all([
+            sourceAccount.save(),
+            destinationAccount.save()
+        ]);
+
         const transaction = new Transaction(transactionData);
         await transaction.save();
 
